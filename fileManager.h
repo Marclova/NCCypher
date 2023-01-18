e85dabaadcf4a5a1876ccee728c37130afe0dc3d
Marclova <marco.cocilova@studenti.unicam.it>
2023-01-18 18:37:49 +0100

    [MINOR] changed the file paths and some prinf texts.

--- fileManager.h
+++ fileManager.h
@@ -1,12 +1,12 @@
 #include "NCCypher.h"
 
-#define inputPath "/home/redh/Desktop/CLionProjects/NCCypherProject/cmake-build-debug/input.txt"  //full path del file input
-#define outputPath "/home/redh/Desktop/CLionProjects/NCCypherProject/cmake-build-debug/output.txt"  //full path del file output
+#define inputPath "input.txt"  //relative path del file input
+#define outputPath "output.txt"  //relative path del file output
 #define myMacAddr "00:0c:29:2c:61:79"
 
 long getFileSize();
 
-char macAddress[17] = myMacAddr;
+//char macAddress[17] = myMacAddr;
 
 /**
  *  Questa funzione fa partire il cifratore, sia per cifrare che per decifrare
@@ -14,9 +14,12 @@ char macAddress[17] = myMacAddr;
  * @param mode   determina se cifrare (mode==1) o decifrare (mode==-1)
  */
 void start(short mode) {      //questa funzione non rispetta il principio DRY a causa di problemi con le chiamate
-    //FILE *macAddr = fopen("/sys/class/net/ens33/address.txt","r");        //non lo uso perché questo programma non è autorizzato a leggere questo file
-    //fscanf(macAddr,"%s",macAddress);
-    setCypherWithMacAddress(macAddress);
+    /*char myMacAddr[17];
+    FILE *macAddr = fopen("/sys/class/net/ens33/address.txt","r");        //non lo uso perché questo programma non è autorizzato a leggere questo file
+    fscanf(macAddr,"%s",myMacAddr);
+    printf("%s\n", myMacAddr);*/
+    setCypherWithMacAddress(myMacAddr);
+
     FILE *inputFile = fopen(inputPath, "r");
     long textSize = getFileSize(inputPath);      //ottengo la dimensione del messaggio per poi assegnare il valore alla costante che lo conterrà
     char textRead[textSize];
@@ -27,12 +30,12 @@ void start(short mode) {      //questa funzione non rispetta il principio DRY a 
         printf("%c",textRead[i]); //scrivo il messaggio originale, un carattere alla volta
         textRead[i] = cypherOneCharacter(textRead[i], mode);  //cifra ogni carattere uno alla volta
     }
-    if (mode==1) {printf("\nla frase criptata è:\n");}
-    else {printf("\nla frase decriptata è:\n");}
+    if (mode==1) {printf("\nla frase cifrata è:\n");}
+    else {printf("\nla frase decifrata è:\n");}
     printf("%s\n\n", textRead);
     FILE *outputFile = fopen(outputPath, "w");    //scrivo il risultato nel file "output.txt"
-    fprintf(outputFile, textRead),
-            fclose(inputFile);
+    fprintf(outputFile, textRead);
+    fclose(inputFile);
     fclose(outputFile);
 }
 
