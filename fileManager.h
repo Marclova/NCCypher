09bc53d676a413fb67705b72bba1f9593b068968
Marclova <marco.cocilova@studenti.unicam.it>
2023-01-31 10:58:59 +0100

    [FIX] Added handling in case of MAC address not found. Now it will work anyway with default values.

--- fileManager.h
+++ fileManager.h
@@ -4,21 +4,21 @@
 
 #define inputPath "input.txt"  //relative path del file input
 #define outputPath "output.txt"  //relative path del file output
-//#define myMacAddr "00:0c:29:2c:61:79"
 
 long getFileSize();
-
-//char macAddress[17] = myMacAddr;
 
 /**
  *  Questa funzione fa partire il cifratore, sia per cifrare che per decifrare
  *
  * @param mode   determina se cifrare (mode==1) o decifrare (mode==-1)
  */
-void start(short mode) {      //questa funzione non rispetta il principio DRY a causa di problemi con le chiamate
+void start(short mode) {
     char myMacAddr[17];
     getMacAddress(myMacAddr);
-    printf("il tuo indirizzo MAC è: %s\n",myMacAddr);
+    if( strlen(myMacAddr) > 2)      //utilizzo il numero 2 perché una stringa vuota risulta di tale lunghezza
+        printf("il tuo indirizzo MAC è: %s\n",myMacAddr);
+    else
+        printf("Non è stato trovato alcun indirizzo MAC. L'operazione userà la chiave di default...");
     setCypherWithMacAddress(myMacAddr);
 
     FILE *inputFile = fopen(inputPath, "r");
@@ -31,8 +31,10 @@ void start(short mode) {      //questa funzione non rispetta il principio DRY a 
         printf("%c",textRead[i]); //scrivo il messaggio originale, un carattere alla volta
         textRead[i] = cypherOneCharacter(textRead[i], mode);  //cifra ogni carattere uno alla volta
     }
-    if (mode==1) {printf("\nla frase cifrata è:\n");}
-    else {printf("\nla frase decifrata è:\n");}
+    if (mode==1)
+        printf("\nla frase cifrata è:\n");
+    else
+        printf("\nla frase decifrata è:\n");
     printf("%s\n\n", textRead);
     FILE *outputFile = fopen(outputPath, "w");    //scrivo il risultato nel file "output.txt"
     fprintf(outputFile, textRead);
