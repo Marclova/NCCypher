377ce8567d84a6ea36eda5a6ef7b0d421232231c
Marclova <marco.cocilova@studenti.unicam.it>
2023-01-31 10:14:58 +0100

    [UPDATE] Removed useless instance.

--- MacAddressProvider.h
+++ MacAddressProvider.h
@@ -6,21 +6,21 @@
 
 
 void getMacAddress(char *mac_address) {
-    struct ifaddrs *ifaddr, *ifa;       //Creo le due struct che conterranno i dati delle interfacce
+    struct ifaddrs *ifaddr;       //Creo le due struct che conterranno i dati delle interfacce
 
     if (getifaddrs(&ifaddr) == -1) {    //il metodo crea una lista collegata di struct ifaddrs, inizializzando *ifaddr
         perror("Error getting interfaces");
         return;
     }
 
-    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {      //iterazione su tutte le interfacce
-        if (ifa->ifa_addr == NULL)      //se l'interaffia corrrente non contiene i dati richiesti, essa viene scartata
+    for (; ifaddr != NULL; ifaddr = ifaddr->ifa_next) {      //iterazione su tutte le interfacce
+        if (ifaddr->ifa_addr == NULL)      //se l'interaffia corrrente non contiene i dati richiesti, essa viene scartata
             continue;
 
-        if ((ifa->ifa_flags & IFF_UP) && (ifa->ifa_addr->sa_family == AF_PACKET)) {     //controllo se l'interfaccia è attiva ed è un AF_PACKET
-            char *interface_name = ifa->ifa_name;
+        if ((ifaddr->ifa_flags & IFF_UP) && (ifaddr->ifa_addr->sa_family == AF_PACKET)) {     //controllo se l'interfaccia è attiva ed è un AF_PACKET
+            char *interface_name = ifaddr->ifa_name;
             if (interface_name[0] == 'e' || interface_name[0] == 'w') {
-                struct sockaddr_ll *s = (struct sockaddr_ll*)ifa->ifa_addr;     //cast da iffadrs a sockaddr_ll
+                struct sockaddr_ll *s = (struct sockaddr_ll*)ifaddr->ifa_addr;     //cast da iffadrs a sockaddr_ll
                 for (int i = 0; i < 6; i++) {        //estrazione del valore MAC un byte alla volta...
                     sprintf(mac_address+i*3, "%02X:", s->sll_addr[i]);  //...perché è necessario costruire una sringa
                 }
@@ -29,5 +29,4 @@ void getMacAddress(char *mac_address) {
             }
         }
     }
-    freeifaddrs(ifaddr);        //liberazione dello spazzio utilizzato
 }
